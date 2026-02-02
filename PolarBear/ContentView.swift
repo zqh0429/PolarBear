8eb776b4f174b4bdb5f2ad99b813da7b2fd07d4a
zqh0429 <1627212412@qq.com>
2026-02-02 21:59:45 +0800

    Initial Commit

---
+++ PolarBear/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PolarBear
+//
+//  Created by Dawnnn on 2026/2/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PolarBear/PolarBearApp.swift
@@ -0,0 +1,17 @@
+//
+//  PolarBearApp.swift
+//  PolarBear
+//
+//  Created by Dawnnn on 2026/2/2.
+//
+
+import SwiftUI
+
+@main
+struct PolarBearApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
